194e78e133af0476a82e0b5dfc2f2871e696fdf7
yywd_123 <528839006@qq.com>
2022-03-25 11:34:02 +0000

    64 bit supported

--- sources/boot.asm
+++ sources/boot.asm
@@ -1,27 +1,61 @@
 ;multiboot header for EXOS
 ;Copyright (C) 2020-2022 yywd_123
-;date: 2022-3-23
+;date: 2022-3-25
 
-section .kernel_header
+section .text
+global start, _start
+extern KernelInit
+
+start:
+_start:
+  jmp KernelEntry
+
+  align 8
+
 header_start:
   dd 0xe85250d6     ;multiboot2
   dd 0              ;i386 protected mode
   dd header_end - header_start  ;header length
-  dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start)) ;checksum
+  dd -(0xe85250d6 + 0 + (header_end - header_start))  ;checksum
   
+addr_tag_start:
+  db 2
+  db 1
+  dd addr_tag_end - addr_tag_start
+  dd _start
+  dd 0x0
+  dd 0x0
+addr_tag_end:
+entry_addr_tag_start:
+  db 3
+  db 1
+  dd entry_addr_tag_end - entry_addr_tag_start
+  dd KernelEntry
+entry_addr_tag_end:
+framefuffer_tag_start:
+  db 5
+  db 1
+  dd framefuffer_tag_end - framefuffer_tag_start
+  dd 1024
+  dd 768
+  dd 32
+framefuffer_tag_end:
   ;tag end
   dw 0
   dw 0
   dd 8
 header_end:
-
-section .kernel_loader
-global start
-
 [bits 32]
+KernelEntry:
+  mov esp, (stack + 0x4000)
 
-start:
-  mov dword [0xb8000], 0x2f4b2f4f
-  hlt
-  
-  
+  push 0
+  popf
+
+  push ebx
+  push eax
+
+  call KernelInit
+
+section .bss
+  stack resb  0x4000
