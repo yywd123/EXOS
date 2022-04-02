7792e45fe0bf339211e32c31f9a16a77df5b12d8
yywd_123 <528839006@qq.com>
2022-04-02 13:49:44 +0800

    OSBase.h

---
+++ sources/lib/OSBase.h
@@ -0,0 +1,17 @@
+#include <multiboot2.h>   //always first
+
+// variable
+struct multiboot_tag_basic_meminfo *meminfo;
+struct multiboot_tag_framebuffer *fbtag;
+struct multiboot_tag_vbe *vbetag;
+struct vbe_info_block *vbe_info;
+struct vbe_mode_info_block *vbe_modeinfo;
+
+// typedef
+typedef struct 
+{
+   
+}
+
+// function libs
+#include <display/framebuffer.c>
