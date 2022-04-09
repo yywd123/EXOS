ac18928918b081a70b0a96d7e210c5c4d86492a2
yywd_123 <528839006@qq.com>
2022-04-09 14:31:17 +0800

    test success

--- sources/boot/boot.asm
+++ sources/boot/boot.asm
@@ -16,33 +16,6 @@ header_start:
   dd 0              ;i386 protected mode
   dd header_end - header_start  ;header length
   dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-  
-  align 8
-  info_request_tag:
-    dw 1
-    dw 0
-    dd info_request_tag_end - info_request_tag
-    dd 4            ;request basic_meminfo 
-  info_request_tag_end:
-
-  align 8
-  addr_tag:
-    dw 2
-    dw 1
-    dd addr_tag_end - addr_tag
-    dd header_start
-    dd KernelEntry
-    dd 0
-    dd 0
-  addr_tag_end:
-      
-  align 8
-  entry_tag:
-    dw 3
-    dw 1
-    dd entry_tag_end - entry_tag
-    dd KernelEntry
-  entry_tag_end:
 
   align 8
   framebuffer_tag:
@@ -60,20 +33,21 @@ header_start:
   dd 8
 header_end:
 KernelEntry:
+  ;Init Stack
   mov esp, (Stack + Stack_Size)
 
   push 0
   popf
 
   push ebx
-  push eax
-
   call KernelInit
-  cmp eax, -1
-  je error
+
+  hlt
 
 error:
   hlt
   jmp error
+
 section .bss
-Stack  resb  Stack_Size
+  Stack  resb  Stack_Size
+  
