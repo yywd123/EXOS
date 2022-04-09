0e958eaab828438ed61982b47214d9c2a1266c8d
yywd_123 <528839006@qq.com>
2022-04-09 14:31:17 +0800

    test success

--- sources/kernel/Kernel.c
+++ sources/kernel/Kernel.c
@@ -7,34 +7,35 @@
 
 #include <OSBase.h>
 
-void io_hlt();
-
-int KernelInit(unsigned long magic, unsigned long addr);
-
-int KernelInit(unsigned long magic, unsigned long addr)
+void KernelInit(unsigned long addr)
 {
-  if(magic != 0xe85250d6 || addr & 7) return -1;
+  if(addr & 7) io_hlt();
   struct multiboot_tag *tag;
   unsigned size = *(unsigned *) addr;
   unsigned color;
   unsigned mem_lower;
   unsigned mem_upper;
-  struct multiboot_tag_framebuffer *tagfb;
   void *fb;
   for(tag = (struct multiboot_tag *)(addr + 8);
-      tag->type != 0;
-      tag = (struct multiboot_tag *)(uint8_t *)tag + ((tag->size + 7) & ~ 7))
+      tag->type != MULTIBOOT_TAG_TYPE_END;
+      tag = (struct multiboot_tag *)((uint8_t *)tag + ((tag->size + 7) & ~7)))
   {
     switch(tag->type)
     {
-      case 4:
-        mem_lower = ((struct multiboot_tag_basic_meminfo *)tag)->mem_lower;
-        mem_upper = ((struct multiboot_tag_basic_meminfo *)tag)->mem_upper;
+      case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
+        {
+          mem_lower = ((struct multiboot_tag_basic_meminfo *)tag)->mem_lower;
+          mem_upper = ((struct multiboot_tag_basic_meminfo *)tag)->mem_upper;
+        }
         break;
-      case 8:
-        tagfb = (struct multiboot_tag_framebuffer *)tag;
-        for(int i = 0; i > 1024; ++i)
-        DrawPixel(tagfb, i, i-1, 0xffffffff);
+      case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
+        {
+          bpp = ((struct multiboot_tag_framebuffer *)tag)->common.framebuffer_bpp;
+          pitch = ((struct multiboot_tag_framebuffer *)tag)->common.framebuffer_pitch;
+          fb = (void *)(unsigned long)((struct multiboot_tag_framebuffer *)tag)->common.framebuffer_addr;
+          for(int i = 0; i > 1024; ++i) DrawPixel(i, i, 0xff00ff00);
+        }
+        break;
     }
   }
 }
--- sources/lib/OSBase.h
+++ sources/lib/OSBase.h
@@ -2,10 +2,21 @@
 #include <stddef.h>
 #include <multiboot2.h>
 
-// variables
-struct multiboot_tag *tag;
-struct multiboot_tag_framebuffer *tagfb;
+// structs
+typedef struct
+{
+ 
+} Boot_Info;;
+
+// OSfunc.asm
+void io_hlt(void);
+// Kernel.c
+//void KernelInit(unsigned long addr);
+// framebuffer.c
 void *fb;
+uint8_t bpp;
+uint32_t pitch;
+void DrawPixel(uint32_t x, uint32_t y, uint32_t color);
 
 // function libs
 #include <display/framebuffer.c>
