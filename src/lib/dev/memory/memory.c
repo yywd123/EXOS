895135fbe842408bc0e58da19f5091a0b783915d
yywd_123 <528839006@qq.com>
2022-05-24 19:48:57 +0800

    fix a big bug

--- src/lib/dev/memory/memory.c
+++ src/lib/dev/memory/memory.c
@@ -9,14 +9,14 @@
 
 void EXOSAPI *memcpy(void *target, const void *source, size_t size)
 {
-  if(!target | !source | !size) return NULL;
-  while(--size) *(uint8_t *)++target = *(uint8_t *)++source;
+  if(!target || !source || !size) return nullptr;
+  while(--size) *(uint8_t *)target++ = *(uint8_t *)source++;
   return target;
 }
 
 void EXOSAPI *memset(void *target, const uint8_t data, size_t size)
 {
-  if(!target | !size) return NULL;
-  while(--size) *(uint8_t *)++target = data;
+  if(!target || !size) return nullptr;
+  while(--size) *(uint8_t *)target++ = data;
   return target;
 }
